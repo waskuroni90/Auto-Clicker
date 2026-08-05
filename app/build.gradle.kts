83d189dbf14deeb1c9b8f787611a15400e2e7878
waskuroni90 <146522645+waskuroni90@users.noreply.github.com>
2026-08-05 22:14:23 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
+     
     }
   }
   compileOptions {
